451b403de5f844c33bcb1c42f14e94d7db83c9af
Willi Krappen <wkrappen91@yahoo.de>
2020-05-16 16:40:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react";
+import { useRecoilValue } from "recoil";
+
+function useLoggedState(atom) {
+  const currentValue = useRecoilValue(atom);
+  const previousValue = usePrevious(currentValue);
+
+  useEffect(() => {
+    console.log(
+      "%c" + atom.key + " updating",
+      "color: green; font-weight: bold"
+    );
+    console.log("Old value: ", previousValue);
+    console.log("New value: ", currentValue);
+    console.log("%c--------------------", "color: #555;");
+  }, [currentValue]);
+}
+
+function useRecoilLogger(...atoms) {
+  atoms.forEach((atom) => {
+    useLoggedState(atom);
+  });
+}
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+export function RecoilLogger({ atoms = [], children = null }) {
+  useRecoilLogger(...atoms);
+  return children;
+}
+
+export default RecoilLogger;
